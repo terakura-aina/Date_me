06fc550b5228ec62b87f078b7f443801c2e2a458
terakura aina <aicaierwei@gmail.com>
2021-02-15 13:31:12 +0900

    schedules#editページにOKとNGボタンを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'make_plans/create'
   resources :users, only: [:create]
-  resources :schedules, only: [:create, :new, :update, :index]
+  resources :schedules, only: [:create, :new, :update, :index, :destroy]
   resources :make_plans, only: [:create]
-  
+
   root 'schedules#show'
   get '/schedules/:token' => 'schedules#edit'
 end
