8636e55200e98a1e409bab7e5f8464a59b6fd360
terakura aina <aicaierwei@gmail.com>
2021-02-15 12:14:42 +0900

    make_plansコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'make_plans/create'
   resources :users, only: [:create]
   resources :schedules, only: [:create, :new, :update, :index]
   resources :make_plans, only: [:create]
