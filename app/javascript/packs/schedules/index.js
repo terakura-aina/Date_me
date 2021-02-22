3f5dd43f6325f778d877298930dbecfc6f5ec2da
terakura aina <aicaierwei@gmail.com>
2021-02-22 15:41:41 +0900

    スケジュール一覧ページ実装

--- app/javascript/packs/schedules/index.js
+++ app/javascript/packs/schedules/index.js
@@ -4,6 +4,26 @@ document.addEventListener('DOMContentLoaded', () => {
   liff.init({
     liffId: "1655665365-robLXJ1P"
   })
+  // idTokenからユーザーIDを取得し、userテーブルに保存するための処理
+  .then(() => {
+    const idToken = liff.getIDToken()
+    console.log(idToken)
+    const body =`idToken=${idToken}`
+    const request = new Request('/users', {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+        'X-CSRF-Token': token
+      },
+      method: 'POST',
+      body: body
+    });
+
+    fetch(request)
+    .then(response => response.json())
+    .then(data => {
+      data_id = data.id
+    })
+  })
   .then(() => {
     if (!liff.isLoggedIn()) {
       liff.login();
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+20.times do |n|
+  Mission.create!(
+    body: "mission#{n + 1}",
+  )
+end
