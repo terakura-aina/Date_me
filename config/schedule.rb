8e3278d692740f5d65e5bb9d08366800957815b0
terakura aina <aicaierwei@gmail.com>
2021-03-05 23:24:49 +0900

    schedule.rb設定変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = ENV['RAILS_ENV'] || :production
 
 set :environment, rails_env
+set :output, "#{Rails.root}/log/cron.log"
 
 every 15.minutes do
   rake 'push:push_remind'
