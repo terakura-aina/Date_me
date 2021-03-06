0170368941d657f4879cf9943eda619dd53ee570
terakura aina <aicaierwei@gmail.com>
2021-03-06 16:21:33 +0900

    schedule.rb設定変更

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,8 @@ require File.expand_path(File.dirname(__FILE__) + "/environment")
 rails_env = ENV['RAILS_ENV'] || :production
 
 set :environment, rails_env
-set :output, "#{Rails.root}/log/cron.log"
+set :output, environment == 'development' ? 'log/crontab.log' : '/deploy/apps/Date_me/shared/log/crontab.log'
+job_type :rake, 'export PATH="$HOME/.rbenv/bin:$PATH"; eval "$(rbenv init -)"; cd :path && RAILS_ENV=:environment bundle exec rake :task :output'
 
 every 15.minutes do
   rake 'push:push_remind'
