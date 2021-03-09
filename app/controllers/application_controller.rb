27d3cdc1d7b1217d02688b06d568a178ab517171
terakura aina <aicaierwei@gmail.com>
2021-03-09 17:48:42 +0900

    schedules#indexページの配色を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,10 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user
   # CSRF保護を無効にする
   protect_from_forgery with: :null_session
 
   rescue_from StandardError, with: :render_500
   rescue_from ActiveRecord::RecordNotFound, with: :render_404
   rescue_from ActionController::RoutingError, with: :render_404
-
-  def current_user
-    if session[:user_id]
-      #値がnilであればUser.find_by(id: session[:user_id])が代入される
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
-  end
 
   private
 
